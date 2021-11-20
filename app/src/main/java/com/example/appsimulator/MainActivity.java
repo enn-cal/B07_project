70db73b1e1bd8dd2dfb4c2d3f84dde42c29a2e1c
Nehchal Kalsi <niks.calsy@gmail.com>
2021-11-20 08:06:05 -0400

    Connect MainAcivity and RegisterUser

--- app/src/main/java/com/example/appsimulator/MainActivity.java
+++ app/src/main/java/com/example/appsimulator/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appsimulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+
+    public void registerNewUser(View v)
+    {
+        Intent intent = new Intent(this, RegisterUser.class);
+        startActivity(intent);
+//        TextView tv= (TextView) findViewById(R.id.text_view);
+//
+//        //alter text of textview widget
+//        tv.setText("This text view is clicked");
+//
+//        //assign the textview forecolor
+//        tv.setTextColor(Color.GREEN);
+    }
 
 }
