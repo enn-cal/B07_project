f0e33396ab69ce2f9c3e3cf05cfc40cd1b420b32
Nehchal Kalsi <niks.calsy@gmail.com>
2021-11-29 04:12:19 -0400

    implement sign out

--- app/src/main/java/com/example/appsimulator/CustomerScreen.java
+++ app/src/main/java/com/example/appsimulator/CustomerScreen.java
@@ -1,14 +1,21 @@
 package com.example.appsimulator;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +31,9 @@ public class CustomerScreen extends AppCompatActivity {
     private Button myCart;
     private Button signOut;
     private RecyclerView recyclerView;
+//    private FirebaseAuth.AuthStateListener authStateListener;
+//    private  FirebaseAuth authStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +66,77 @@ public class CustomerScreen extends AppCompatActivity {
             }
         });
 
+//        signOut = (Button) findViewById(R.id.button6);
+////        setSignOut();
+////        authStateListener = FirebaseAuth.getInstance();
+//
+//
+//        signOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FirebaseAuth.getInstance().signOut();
+////            setSignOut();
+//            }
+//        });
+
 
     }
+//
+//    private void setSignOut() {
+////        authStateListener.signOut();
+////        authStateListener = new FirebaseAuth.AuthStateListener() {
+//        FirebaseAuth.AuthStateListener listener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                if(firebaseAuth.getCurrentUser() == null){
+//                    //logout successful
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(CustomerScreen.this, R.style.DialogStyle);
+//                    builder.setMessage("Logout successful !!");
+//                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialogInterface, int i) {
+//                            finish();
+//                        }
+//                    });
+//                    builder.show();
+//                }else{
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(CustomerScreen.this, R.style.DialogStyle);
+//                    builder.setMessage("Oops..please try again !!");
+//                    builder.setPositiveButton("OK", null);
+//                    builder.show();
+//                }
+//            }
+//        };
+////        authStateListener = new FirebaseAuth.AuthStateListener() {
+////            @Override
+////            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+////                FirebaseUser user = firebaseAuth.getCurrentUser();
+////
+////                if (user != null) {
+////                    Log.d("CustomerScreen", "onAuthStateChanged: signed_in: " + user.getUid());
+////                } else {
+////                    Log.d("CustomerScreen", "onAuthChanged: signed_out");
+////                    Toast.makeText(CustomerScreen.this, "Signed Out", Toast.LENGTH_SHORT).show();
+////                    Intent intent = new Intent(CustomerScreen.this, MainActivity.class);
+////                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//////                    startActivity(intent);
+////                }
+////            }
+////        };
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        FirebaseAuth.getInstance().addAuthStateListener(authStateListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        // remove lister to prevent it listening at all times
+//        if(authStateListener != null) {
+//            FirebaseAuth.getInstance().removeAuthStateListener(authStateListener);
+//        }
+//    }
 }
